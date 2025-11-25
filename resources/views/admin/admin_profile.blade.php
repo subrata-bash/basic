4557358d0fa4f2943a241deb14c3057cd9d096a4
Om Sri Guru Joy <github.subrata@gmail.com>
2025-11-26 02:06:37 +0530

    Admin Profile and Image Update Part 3

--- resources/views/admin/admin_profile.blade.php
+++ resources/views/admin/admin_profile.blade.php
@@ -1,5 +1,7 @@
 @extends('admin.admin_master')
 @section('admin')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
     <div class="content">
         <!-- Start Content-->
         <div class="container-xxl">
@@ -90,15 +92,16 @@
                                                             <div class="form-group mb-3 row">
                                                                 <label class="form-label">Photo</label>
                                                                 <div class="col-lg-12 col-xl-12">
-                                                                    <input class="form-control" name="photo"
-                                                                        type="file">
+                                                                    <input id="image" class="form-control"
+                                                                        name="photo" type="file">
                                                                 </div>
                                                             </div>
 
                                                             <div class="form-group mb-3 row">
                                                                 <label class="form-label"></label>
                                                                 <div class="col-lg-12 col-xl-12">
-                                                                    <img src="{{ !empty($profileData->photo) ? url('upload/user_images' . $profileData->photo) : url('upload/no_image.jpg') }}"
+                                                                    <img id="showImage"
+                                                                        src="{{ !empty($profileData->photo) ? url('upload/user_images' . $profileData->photo) : url('upload/no_image.jpg') }}"
                                                                         class="rounded-circle avatar-xxl img-thumbnail float-start"
                                                                         alt="image profile">
                                                                 </div>
@@ -174,4 +177,16 @@
             </div>
         </div>
     </div>
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#image').change(function(e) {
+                var reader = new FileReader();
+                reader.onload = function(e) {
+                    $('#showImage').attr('src', e.target.result);
+                }
+                reader.readAsDataURL(e.target.files['0']);
+            })
+        })
+    </script>
 @endsection
